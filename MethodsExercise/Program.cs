ef990df176401c1fc4b27fec9cebbc30bbc5b2f6
Reashad Zaman <zamanshad118@gmail.com>
2026-02-10 22:02:34 -0500

    Story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StoryTeller();
+            
+        }
+
+        public static void StoryTeller()
+        {
+            Console.WriteLine("What is your name ?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What is your favourite color ?");
+            string color = Console.ReadLine();
+            
+            Console.WriteLine("Name one favourite music band or artist of yours ");
+            string music = Console.ReadLine();
+            
+            Console.WriteLine("What is your favourite restaurant ?");
+            string rest = Console.ReadLine();
+            
+            Console.WriteLine($"{name} sits at {rest} dressed in {color}, relaxed and composed.\n{music}plays through headphones while a warm meal is enjoyed.Music, style, and flavor come together in a perfectly balanced moment.");
+            
         }
     }
 }
